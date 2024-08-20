4e927326f5fafc777807993bb0b136563716cf45
kazzenaazalen <105841107122@student.unismuh.ac.id>
2024-08-20 18:49:29 +0800

    selesai chapter 8

--- app/lib/data.js
+++ app/lib/data.js
@@ -3,23 +3,17 @@ import { formatCurrency } from './utils';
 
 export async function fetchRevenue() {
   try {
-    // Artificially delay a response for demo purposes.
-    // Don't do this in production :)
-
     console.log('Fetching revenue data...');
     await new Promise((resolve) => setTimeout(resolve, 3000));
-
     const data = await sql`SELECT * FROM revenue`;
-
     console.log('Data fetch completed after 3 seconds.');
-
     return data.rows;
   } catch (error) {
-    console.error('Database Error:', error);
+    console.error('Database Error:', error.message);
+    console.error('Stack Trace:', error.stack);
     throw new Error('Failed to fetch revenue data.');
   }
 }
-
 export async function fetchLatestInvoices() {
   try {
     const data = await sql`
